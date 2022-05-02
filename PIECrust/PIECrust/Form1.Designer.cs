c5cac3e529a4c7c102589ccc0f94e547ff487e57
peteradanko <56607491+peteradanko@users.noreply.github.com>
2022-05-01 20:12:49 -0400

    Add files via upload

---
+++ PIECrust/PIECrust/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace PIECrust
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBoxProblem = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxImpact = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxExpectation = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxResolution = new System.Windows.Forms.RichTextBox();
+            this.labelEnvironment = new System.Windows.Forms.Label();
+            this.labelProduct = new System.Windows.Forms.Label();
+            this.labelImpact = new System.Windows.Forms.Label();
+            this.labelExpectation = new System.Windows.Forms.Label();
+            this.labelResolution = new System.Windows.Forms.Label();
+            this.labelProblem = new System.Windows.Forms.Label();
+            this.textBoxEnvironment = new System.Windows.Forms.TextBox();
+            this.textBoxProduct = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.buttonIRT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PIE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBoxProblem
+            // 
+            this.richTextBoxProblem.Location = new System.Drawing.Point(89, 88);
+            this.richTextBoxProblem.Name = "richTextBoxProblem";
+            this.richTextBoxProblem.Size = new System.Drawing.Size(255, 83);
+            this.richTextBoxProblem.TabIndex = 5;
+            this.richTextBoxProblem.Text = "";
+            // 
+            // richTextBoxImpact
+            // 
+            this.richTextBoxImpact.Location = new System.Drawing.Point(89, 177);
+            this.richTextBoxImpact.Name = "richTextBoxImpact";
+            this.richTextBoxImpact.Size = new System.Drawing.Size(255, 83);
+            this.richTextBoxImpact.TabIndex = 6;
+            this.richTextBoxImpact.Text = "";
+            // 
+            // richTextBoxExpectation
+            // 
+            this.richTextBoxExpectation.Location = new System.Drawing.Point(89, 266);
+            this.richTextBoxExpectation.Name = "richTextBoxExpectation";
+            this.richTextBoxExpectation.Size = new System.Drawing.Size(255, 83);
+            this.richTextBoxExpectation.TabIndex = 7;
+            this.richTextBoxExpectation.Text = "";
+            // 
+            // richTextBoxResolution
+            // 
+            this.richTextBoxResolution.Location = new System.Drawing.Point(350, 88);
+            this.richTextBoxResolution.Name = "richTextBoxResolution";
+            this.richTextBoxResolution.Size = new System.Drawing.Size(255, 83);
+            this.richTextBoxResolution.TabIndex = 8;
+            this.richTextBoxResolution.Text = "";
+            // 
+            // labelEnvironment
+            // 
+            this.labelEnvironment.AutoSize = true;
+            this.labelEnvironment.Location = new System.Drawing.Point(8, 13);
+            this.labelEnvironment.Name = "labelEnvironment";
+            this.labelEnvironment.Size = new System.Drawing.Size(75, 15);
+            this.labelEnvironment.TabIndex = 9;
+            this.labelEnvironment.Text = "Environment";
+            // 
+            // labelProduct
+            // 
+            this.labelProduct.AutoSize = true;
+            this.labelProduct.Location = new System.Drawing.Point(34, 52);
+            this.labelProduct.Name = "labelProduct";
+            this.labelProduct.Size = new System.Drawing.Size(49, 15);
+            this.labelProduct.TabIndex = 10;
+            this.labelProduct.Text = "Product";
+            // 
+            // labelImpact
+            // 
+            this.labelImpact.AutoSize = true;
+            this.labelImpact.Location = new System.Drawing.Point(39, 180);
+            this.labelImpact.Name = "labelImpact";
+            this.labelImpact.Size = new System.Drawing.Size(44, 15);
+            this.labelImpact.TabIndex = 11;
+            this.labelImpact.Text = "Impact";
+            // 
+            // labelExpectation
+            // 
+            this.labelExpectation.AutoSize = true;
+            this.labelExpectation.Location = new System.Drawing.Point(14, 269);
+            this.labelExpectation.Name = "labelExpectation";
+            this.labelExpectation.Size = new System.Drawing.Size(69, 15);
+            this.labelExpectation.TabIndex = 12;
+            this.labelExpectation.Text = "Expectation";
+            // 
+            // labelResolution
+            // 
+            this.labelResolution.AutoSize = true;
+            this.labelResolution.Location = new System.Drawing.Point(350, 67);
+            this.labelResolution.Name = "labelResolution";
+            this.labelResolution.Size = new System.Drawing.Size(63, 15);
+            this.labelResolution.TabIndex = 13;
+            this.labelResolution.Text = "Resolution";
+            // 
+            // labelProblem
+            // 
+            this.labelProblem.AutoSize = true;
+            this.labelProblem.Location = new System.Drawing.Point(31, 91);
+            this.labelProblem.Name = "labelProblem";
+            this.labelProblem.Size = new System.Drawing.Size(52, 15);
+            this.labelProblem.TabIndex = 14;
+            this.labelProblem.Text = "Problem";
+            // 
+            // textBoxEnvironment
+            // 
+            this.textBoxEnvironment.Location = new System.Drawing.Point(89, 10);
+            this.textBoxEnvironment.Name = "textBoxEnvironment";
+            this.textBoxEnvironment.Size = new System.Drawing.Size(255, 23);
+            this.textBoxEnvironment.TabIndex = 15;
+            // 
+            // textBoxProduct
+            // 
+            this.textBoxProduct.Location = new System.Drawing.Point(89, 49);
+            this.textBoxProduct.Name = "textBoxProduct";
+            this.textBoxProduct.Size = new System.Drawing.Size(255, 23);
+            this.textBoxProduct.TabIndex = 16;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(434, 10);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(171, 23);
+            this.textBoxName.TabIndex = 17;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(353, 13);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(42, 15);
+            this.labelName.TabIndex = 18;
+            this.labelName.Text = "Name:";
+            // 
+            // buttonIRT
+            // 
+            this.buttonIRT.Location = new System.Drawing.Point(113, 355);
+            this.buttonIRT.Name = "buttonIRT";
+            this.buttonIRT.Size = new System.Drawing.Size(75, 23);
+            this.buttonIRT.TabIndex = 19;
+            this.buttonIRT.Text = "IRT";
+            this.buttonIRT.UseVisualStyleBackColor = true;
+            this.buttonIRT.Click += new System.EventHandler(this.buttonIRT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonIRT);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxProduct);
+            this.Controls.Add(this.textBoxEnvironment);
+            this.Controls.Add(this.labelProblem);
+            this.Controls.Add(this.labelResolution);
+            this.Controls.Add(this.labelExpectation);
+            this.Controls.Add(this.labelImpact);
+            this.Controls.Add(this.labelProduct);
+            this.Controls.Add(this.labelEnvironment);
+            this.Controls.Add(this.richTextBoxResolution);
+            this.Controls.Add(this.richTextBoxExpectation);
+            this.Controls.Add(this.richTextBoxImpact);
+            this.Controls.Add(this.richTextBoxProblem);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PieCrust";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBoxProblem;
+        private System.Windows.Forms.RichTextBox richTextBoxImpact;
+        private System.Windows.Forms.RichTextBox richTextBoxExpectation;
+        private System.Windows.Forms.RichTextBox richTextBoxResolution;
+        private System.Windows.Forms.Label labelEnvironment;
+        private System.Windows.Forms.Label labelProduct;
+        private System.Windows.Forms.Label labelImpact;
+        private System.Windows.Forms.Label labelExpectation;
+        private System.Windows.Forms.Label labelResolution;
+        private System.Windows.Forms.Label labelProblem;
+        private System.Windows.Forms.TextBox textBoxEnvironment;
+        private System.Windows.Forms.TextBox textBoxProduct;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Button buttonIRT;
+    }
+}
+
---
+++ PIECrust/PIECrust/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIECrust
+{
+    public partial class Form1 : Form
+    {
+        string Resolution = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if(richTextBoxResolution.Text == "")
+            {
+                Resolution = "To Be Determined";
+            }else
+            {
+                Resolution = richTextBoxResolution.Text;
+            }
+            
+            Clipboard.SetText("****************************************"
+                + "\n" + "*ENVIRONMENT*" + "\n\n" + textBoxEnvironment.Text
+                + "\n\n" +"------------------------------------"
+                + "\n\n" + "*PROBLEM*"
+                + "\n\n" + "Product: " + textBoxProduct.Text
+                + "\n\n" + richTextBoxProblem.Text
+                + "\n\n" + "------------------------------------"
+                + "\n\n" + "*IMPACT*"
+                + "\n\n" + richTextBoxImpact.Text
+                + "\n\n" + "------------------------------------"
+                + "\n\n" + "*EXPECTATION*"
+                + "\n\n" + richTextBoxExpectation.Text
+                + "\n\n" + "------------------------------------"
+                + "\n\n" + "*RESOLUTION*"
+                + "\n\n" + Resolution);
+        }
+
+        private void buttonIRT_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Hello " + textBoxName.Text
+                + "\n\n" + $"Thank you for opening this ticket with us. Before we get started, I wanted to confirm that the problem you are experiencing with {textBoxProduct.Text} can be summarized as follows:"
+                + "\n\n" + richTextBoxProblem.Text
+                + "\n\n" + "I also wanted to confirm that the impact to you as a customer can be quantified by the following statement"
+                + "\n\n" + richTextBoxImpact.Text
+                + "\n\n" + "Please let me know if I have articulated your expectations for this ticket accurately:"
+                + "\n\n" + richTextBoxExpectation.Text
+                + "\n\n" + "If I've missed anything or interpreted the ticket incorrectly, please let me know. Additionally, please let me know your upcoming schedule so that we may connect."
+                + "\n" + "Thank you!");
+        }
+    }
+}
